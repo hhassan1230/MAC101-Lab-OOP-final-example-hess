2e9f08884d51fbb21843e8daaff5d1339f359650
Hessvacio Hassan <madeinbrooklyngames@gmail.com>
2025-12-16 18:39:14 -0500

    Add weight input and output in questionone.cpp

---
+++ questionone.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    double pounds;
+    std::cout<<"Hello What is you weight in lbs?"<< std::endl;
+    std::cin >> pounds;
+    
+    std::cout<<"Hello you are " << pounds << " lbs?";
+
+
+    return 0;
+}
